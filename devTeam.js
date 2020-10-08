b0a9d012daf4fea493a8233b03d112029453b8af
Christophe-PARENT <christophe.parent2@gmail.com>
2020-10-08 15:21:30 +0200

    modif responsive in home page + modif header in dev team page + add footer in all pages

--- devTeam.js
+++ devTeam.js
@@ -1,21 +1,29 @@
+/* Gestion du header _____________________________________ */
 
-/*const selectImage = document.querySelectorAll("img");
-const selectLi = document.querySelectorAll(".card");
-
-
-
-function changeBorder(i){
-    selectLi[i].style.borderColor = "gold";
-}
-
-function resetBorder(i){
-    selectLi[i].style.borderColor = "darkgray";
-}
-
-for (let i=0 ; i<selectImage.length ; i++){
-
-    selectImage[i].addEventListener("mouseover", changeBorder[i]);
-    selectImage[i].addEventListener("mouseout", resetBorder[i]);
-}
-*/
-
+function displayBurger() {
+    let x = document.getElementById('myLinks');
+    if (x.style.display === 'block') {
+      x.style.display = 'none';
+    } else {
+      x.style.display = 'block';
+    }
+  }
+  
+  const burgerIcon = document.querySelector('.icon');
+  burgerIcon.addEventListener('click', displayBurger);
+  
+  window.addEventListener('resize', function () {
+    const vw = Math.max(
+      document.documentElement.clientWidth || 0,
+      window.innerWidth || 0
+    );
+    let x = document.getElementById('myLinks');
+    if (vw >= 570) {
+      x.style.display = 'flex';
+    } else if (vw < 570) {
+      x.style.display = 'none';
+    }
+  });
+  
+  /* Gestion du header _____________________________________ */
+  
