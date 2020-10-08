4bf5badf02b0a27f4b33436e8d23dc92a9648539
Erberack <brieucquertier73@gmail.com>
2020-10-08 15:26:35 +0200

    Updated calculate function

--- calculate_score.js
+++ calculate_score.js
@@ -3,7 +3,7 @@ let timer = 000;
 function calculate() {
   if (timer > 090) {
     return (score = timer * 2 + ' points');
-  } else if (timer <= 090 && timer >= 001) {
+  } else if (timer <= 090 && timer > 000) {
     return (score = timer + ' points');
   } else {
     return (score = 0 + ' points');
